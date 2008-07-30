9644b12fa8681ef88ac26a009baa0949ab72d228
grebenshikov.n <grebenshikov.n@67635781-b451-0410-a74f-8dd083894bcd>
2008-07-30 11:04:31 +0000

    compiles with mingw32 and linux

---
+++ build_linux.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+mkdir -p build_linux
+cd build_linux
+../configure
+make
---
+++ build_windows.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+mkdir -p build_windows
+cd build_windows
+../configure --enable-builtin-libraries
+make
